1d2b2de0340605992299fc6990a4ebde70c8e3d4
Newman Escobar <newmansayan@gmail.com>
2025-02-27 23:44:29 -0500

    Se agregaron nuevos productos como descripciones, se extendio el tipo de productos para perros y se estaran ajustando las images dependieno del tipo de producto

--- src/js/Alimentos.js
+++ src/js/Alimentos.js
@@ -6,3 +6,27 @@ function mostrarDetalles(id) {
     detalles.style.display = "none";
   }
 }
+
+function mostrarInfo(num) {
+  // Oculta ambas informaciones
+  document.getElementById("info1").style.display = "none";
+  document.getElementById("info2").style.display = "none";
+  document.getElementById("info3").style.display = "none";
+  document.getElementById("info4").style.display = "none";
+  document.getElementById("info5").style.display = "none";
+  document.getElementById("info6").style.display = "none";
+  document.getElementById("info7").style.display = "none";
+  document.getElementById("info8").style.display = "none";
+  document.getElementById("info6").style.display = "none";
+  document.getElementById("info9").style.display = "none";
+  document.getElementById("info10").style.display = "none";
+  document.getElementById("info11").style.display = "none";
+  document.getElementById("info12").style.display = "none";
+  document.getElementById("info13").style.display = "none";
+  document.getElementById("info14").style.display = "none";
+  document.getElementById("info15").style.display = "none";
+  document.getElementById("info16").style.display = "none";
+
+  // Muestra solo la información correspondiente
+  document.getElementById("info" + num).style.display = "block";
+}
